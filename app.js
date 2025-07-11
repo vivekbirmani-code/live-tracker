3a79ca655275b90f34d5bf7f746ee6e5322007ee
Vivek Birmani <vivekbirmani2003@gmail.com>
2025-07-11 21:41:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// import express from 'express';
+// import http from 'http';
+// import path from 'path';  //Helps manage file paths.
+// import socketio from 'socket.io';
+// import { dirname } from 'path';
+
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const { dirname } = require("path");
+
+
+
+
+
+
+const app = express();
+
+const server = http.createServer(app)  // server: A server created using Node’s HTTP module so that it can be used with Socket.IO.
+
+const io = socketio(server)  // This sets up Socket.IO to work with your server. now server and socketio are linked
+
+
+app.set("view engine", "ejs")  // telling Express: I will use EJS files for rendering pages.
+
+app.use(express.static(path.join(__dirname, "public")))
+
+io.on("connection", function(socket){  // socket represents the connected client.
+    socket.on("send-location", function(data){
+        io.emit("recieve-location", {id: socket.id, ...data})
+    })   
+    console.log("Connected")
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id)
+    })
+})
+
+
+
+app.get("/", function (req, res) {
+    res.render("index")
+})
+
+server.listen(3000, function () {
+    console.log("Server started on port 3000");
+})
---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+const socket = io();  // This line connects your browser (client) to the server using Socket.IO.
+
+if(navigator.geolocation){  // Checking for User Location Access, navigator.geolocation is a built-in browser feature.
+    navigator.geolocation.watchPosition((position) => {  // watchPosition keeps tracking the user’s location (not just once, but continuously — like a GPS tracker). It gives you the position object again and again whenever the location changes.
+        const { latitude, longitude } = position.coords
+        socket.emit("send-location", { latitude, longitude })  // This line sends the user's location to the server through Socket.IO, "send-location" is the event name — the server must be listening for this event. { latitude, longitude } is the data you're sending.
+    }, (error) => {
+        console.log(error)
+    },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,  // no caching will be done, means we will need instataneous data
+        timeout: 5000,
+    }
+);
+}
+
+
+const map = L.map("map").setView([0, 0], 16);  // from leaflet we can use L.map, to ask permissions for location permissions
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: "Vivek Birmani"
+}).addTo(map)
+
+
+const markers = {};
+
+socket.on("recieve-location", (data) => {
+    const { id, longitude, latitude } = data;
+    map.setView([latitude, longitude], 16)
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+});
+
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id]
+    }
+})
