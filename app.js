96e5f060b58d58f3a794ff8a0fca4cec70b03b16
Vivek Birmani <vivekbirmani2003@gmail.com>
2025-07-11 21:53:17 +0530

    render changes

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.get("/", function (req, res) {
     res.render("index")
 })
 
-server.listen(3000, function () {
-    console.log("Server started on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
